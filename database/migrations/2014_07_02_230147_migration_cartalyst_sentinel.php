522d8800f6cae9011d89a763c6bc456ef0cf8e81
ahmadfadillllah <ahmadfadillah502@gmail.com>
2023-01-03 11:34:24 +0800

    Update table user

--- database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
+++ database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
@@ -31,6 +31,20 @@ class MigrationCartalystSentinel extends Migration
      */
     public function up()
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('no_hp')->nullable();
+            $table->string('role');
+            $table->string('avatar');
+            $table->string('qrcode')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
         Schema::create('activations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
@@ -94,19 +108,7 @@ class MigrationCartalystSentinel extends Migration
             $table->index('user_id');
         });
 
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('email');
-            $table->string('password');
-            $table->text('permissions')->nullable();
-            $table->timestamp('last_login')->nullable();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
-            $table->timestamps();
 
-            $table->engine = 'InnoDB';
-            $table->unique('email');
-        });
     }
 
     /**
